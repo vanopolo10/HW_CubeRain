17630121cb8c9c14101adfcbade380e2edb6fef6
Vanopolo <56246585+vanopolo10@users.noreply.github.com>
2024-10-26 09:28:33 +0700

    Add files via upload

--- Cube.cs
+++ Cube.cs
@@ -1,48 +1,38 @@
-using System.Collections;
 using UnityEngine;
-using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(Rigidbody))]
 
 public class Cube : MonoBehaviour
 {
-    [SerializeField] private int _minDeathTime = 2;
-    [SerializeField] private int _maxDeathTime = 5;
-    [SerializeField] private float _voidLevel = -7f;
+    [SerializeField] public int MinDeathTime = 2;
+    [SerializeField] public int MaxDeathTime = 5;
     
     private bool _isTouched;
     private Renderer _cubeRenderer;
-
+    private Rigidbody _rigidbody;
+    
+    public void ResetVelocity()
+    {
+        _rigidbody.linearVelocity = Vector3.zero;
+    }
+    
     private void OnEnable()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         _isTouched = false;
         _cubeRenderer = GetComponent<Renderer>();
         _cubeRenderer.material.color = Color.white;
     }
 
-    private void Update()
-    {
-        if (gameObject.transform.position.y < _voidLevel)
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
-        string tag = "Platform";
+        Rigidbody component = collision.gameObject.GetComponent<Rigidbody>();
         
-        if (_isTouched == false && collision.gameObject.CompareTag(tag))
+        if (_isTouched == false && component != null)
         {
             _isTouched = true;
             _cubeRenderer.material.color = Color.red; 
-
-            float randomLifetime = Random.Range(_minDeathTime, _maxDeathTime + 1);
-            StartCoroutine(Die(randomLifetime));
         }
-    }
-
-    private IEnumerator Die(float time)
-    {
-        yield return new WaitForSeconds(time);
-        gameObject.SetActive(false);
     }
 }
--- Spawner.cs
+++ Spawner.cs
@@ -1,61 +1,103 @@
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _cubePrefab;
+    [SerializeField] private Cube _cubePrefab;
     [SerializeField] private int _poolSize = 10;
     [SerializeField] private float _spawnHeight = 10;
     [SerializeField] private float _spawnRate = 0.5f;
-    
-    private Transform _platform;
-    private List<GameObject> _pool;
+    [SerializeField] private bool _autoExpand = false;
+    [SerializeField] private float _voidLevel = -20f;
 
-    void Start()
+    private Transform _platform;
+    private ObjectPool<Cube> _objectPool;
+
+    private void Awake()
     {
         _platform = gameObject.GetComponent<Transform>();
-        _pool = new List<GameObject>();
-        
-        for (int i = 0; i < _poolSize; i++)
-        {
-            GameObject cube = Instantiate(_cubePrefab);
-            cube.SetActive(false); 
-            _pool.Add(cube);
-        }
 
-        InvokeRepeating(nameof(SpawnCube), 0, _spawnRate);
+        _objectPool = new ObjectPool<Cube>(
+            CreatePooledItem,
+            OnTakeFromPool,
+            OnReturnedToPool,
+            OnDestroyPoolObject,
+            _autoExpand,
+            _poolSize);
     }
 
-    void SpawnCube()
+    private void Start()
     {
-        GameObject cube = GetPooledCube();
+        StartCoroutine(SpawnCube(_spawnRate));
+        StartCoroutine(CheckObjectsBelowMinHeight());
+    }
+    
+    IEnumerator CheckObjectsBelowMinHeight(float updateRate = 0.1f)
+    {
+        while (true)
+        {
+            var cubes = FindObjectsByType<Cube>(FindObjectsSortMode.None);
+            foreach (Cube cube in cubes)
+            {
+                if (cube.gameObject.activeInHierarchy && cube.transform.position.y < _voidLevel)
+                {
+                    _objectPool.Release(cube);
+                }
+            }
 
+            yield return new WaitForSeconds(updateRate);
+        }
+    }
+
+    private IEnumerator SpawnCube(float time)
+    {
         int scaleMod = 2;
         float margin = 1;
-        
-        if (cube != null)
+
+        while (true)
         {
+            Cube cube = _objectPool.Get();
+
             Vector3 spawnPosition = _platform.position + new Vector3(
                 Random.Range(-_platform.localScale.x / scaleMod + margin, _platform.localScale.x / scaleMod - margin),
                 _spawnHeight,
                 Random.Range(-_platform.localScale.z / scaleMod + margin, _platform.localScale.z / scaleMod - margin)
             );
-            
+
+            cube.ResetVelocity();
             cube.transform.position = spawnPosition;
-            cube.SetActive(true);
+
+            StartCoroutine(ReturnCubeToPoolAfterTime(cube, Random.Range(cube.MinDeathTime, cube.MaxDeathTime + 1)));
+
+            yield return new WaitForSeconds(time);
         }
     }
 
-    GameObject GetPooledCube()
+    private IEnumerator ReturnCubeToPoolAfterTime(Cube cube, float time)
     {
-        foreach (GameObject cube in _pool)
-        {
-            if (!cube.activeInHierarchy)
-            {
-                return cube;
-            }
-        }
+        yield return new WaitForSeconds(time);
+        _objectPool.Release(cube); 
+    }
 
-        return null;
+
+    private Cube CreatePooledItem()
+    {
+        return Instantiate(_cubePrefab);
+    }
+
+    private void OnTakeFromPool(Cube cube)
+    {
+        cube.gameObject.SetActive(true);
+    }
+
+    private void OnReturnedToPool(Cube cube)
+    {
+        cube.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyPoolObject(Cube cube)
+    {
+        Destroy(cube.gameObject);
     }
 }
